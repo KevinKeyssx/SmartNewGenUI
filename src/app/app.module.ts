921b98174e7d494bd2c1edb34ac3bdd48fa60196
Keyss <55340042+KevinKeyssx@users.noreply.github.com>
2022-02-18 20:36:52 -0300

    Header | Navbar | Constants | Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,19 @@ import { ShoppingComponent }                    from './pages/shopping/shopping.
 import { NotFoundComponent }                    from './pages/not-found/not-found.component';
 import { HttpClientModule, HTTP_INTERCEPTORS }  from '@angular/common/http';
 import { AuthInterceptor }                      from './interceptor/auth.interceptor';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ShoppingComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/util/constants.ts
@@ -0,0 +1,4 @@
+
+export class Constants {
+    public static API_SERVER = 'http://rutaservidor.com/APIREST'; //necesito recoger este valor de MySQL
+}
